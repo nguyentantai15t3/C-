a22e4084c3bdc39733e1bf36ee1cbfbe3aa71e33
nguyentantai15t3 <taijoke97@gmail.com>
2019-06-10 15:20:23 +0700

    struct studient and print menu

--- Pointer_Struct/Project_Struct/Project_Struct.cpp
+++ Pointer_Struct/Project_Struct/Project_Struct.cpp
@@ -1,9 +1,56 @@
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
 
+using namespace std;
 
-int main()
+struct Studient
 {
-    return 0;
+	int id;
+	float score;
+	string name;
+};
+
+void InputStudient(Studient std[])
+{
+
+}
+
+int PrintMenu()
+{
+	int i;
+	cout << "--------------MENU-----------" << endl;
+	cout << "1. Input" << endl;
+	cout << "2. Display" << endl;
+	cout << "3. Save to file " << endl;
+	cout << "4. Load from file" << endl;
+	cout << "0. Exit" << endl;
+	cout << "-----------------------------" << endl;
+	cout << "Choose: ";
+	cin >> i;
+	return i;
+}
+
+void main()
+{
+	//Studient std[100];
+	int choose = PrintMenu();
+	switch (choose)
+	{
+	case 1:
+		//InputStudient();
+	case 2:
+		//Display();
+	case 3: 
+		//SaveToFile();
+	case 4:
+		//LoadToFile();
+	case 0:
+		break;
+	default:
+		break;
+	}
+	system("pause");
 }
 
