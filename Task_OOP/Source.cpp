4639b48b27f6d0468c42e4455538cbcc06b020bf
nguyentantai15t3 <taijoke97@gmail.com>
2019-06-12 16:49:34 +0700

    + code func main()

--- Task_OOP/Source.cpp
+++ Task_OOP/Source.cpp
@@ -1,13 +1,9 @@
 #include "Task_OOP.h"
-#include "Task_OOP.h"
 #include <iostream>
-
-void Input(MyPoint &p)
-{
-	cout << "Input X:";
-}
 
 void main()
 {
-	
+	MyPoint *P1 = new MyPoint(2, 3);
+	MyPoint *P2 = new MyPoint(3, 4);
+	cout << P1->Distance(*P2) << endl;
 }
