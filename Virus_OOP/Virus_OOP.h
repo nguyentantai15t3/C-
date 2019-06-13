8135b455539341f6bcc8fbdf1877cb5df4df26e6
nguyentantai15t3 <taijoke97@gmail.com>
2019-06-13 08:45:25 +0700

    + ADD function and var in class virus

--- Virus_OOP/Virus_OOP.h
+++ Virus_OOP/Virus_OOP.h
@@ -1,1 +1,27 @@
-#pragma once
+﻿#pragma once
+
+class Virus
+{
+public:
+	Virus();
+	Virus(const Virus *virus);
+	~Virus();
+	virtual void LoadADNInformation​();
+	virtual void ReduceResistance​();
+	virtual void DoBorn();
+	virtual void DoClone();
+	virtual void DoDie();
+	virtual void InitResistance();
+
+private:
+	char* m_dna;
+	int m_resistance;
+};
+
+Virus::Virus()
+{
+}
+
+Virus::~Virus()
+{
+}
