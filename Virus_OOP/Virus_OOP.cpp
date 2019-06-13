706bdc3163cb499c01b34a2bf4a9eaf551022d3d
nguyentantai15t3 <taijoke97@gmail.com>
2019-06-13 09:24:00 +0700

    + Add constructor -> Virus cpp

--- Virus_OOP/Virus_OOP.cpp
+++ Virus_OOP/Virus_OOP.cpp
@@ -1,3 +1,18 @@
 #include "stdafx.h"
 #include "Virus_OOP.h"
 
+using namespace std;
+
+Virus::Virus()
+{
+	this->m_dna;
+	this->m_resistance = 10;
+}
+
+Virus::Virus(const Virus* viruss)
+{
+
+}
+
+
+Virus::~Virus() {};
